a23a1bb976a38be8d64b6ad2d263d46661acb1c3
Ryan Irwin <rirwin@bbn.com>
2014-01-29 13:55:48 -0500

    updated this manual fetcher to using the automatic schema approach.

--- aggregator/py-progs/psql_inserts.py
+++ aggregator/py-progs/psql_inserts.py
@@ -4,8 +4,12 @@ import psycopg2
 import json
 import requests
 
-db_schema = {}
-db_schema["memory_util"] = "(aggregate_id varchar, time float8, value float8)"
+
+import sys
+sys.path.append("../../config")
+import db_schema_config
+full_schema = db_schema_config.get_schema()
+
 
 # all in one function for now, simple and updates are contained in
 # here as compared to alternatives (returning a struct that matches
@@ -16,22 +20,51 @@ def json_receiver(json_text, con):
 
     if (data["response_type"] == "data_poll"):
         table_name = data["data_type"] 
-        cur.execute("CREATE TABLE IF NOT EXISTS " + table_name + db_schema[table_name]);
+        table_schema = full_schema[table_name]
+        num_cols = len(table_schema)
+        schema_str = "("
+        for col_i in range(num_cols-1):
+            schema_str = schema_str + table_schema[col_i][0] + " " + table_schema[col_i][1] + ", " 
+        schema_str = schema_str + table_schema[-1][0] + " " + table_schema[-1][1] +  ")";
+       
+        cur.execute("CREATE TABLE IF NOT EXISTS " + table_name + schema_str + ";");
         con.commit()
+        
+        ins_str_base = "INSERT INTO " + table_name + " values "
 
-        print "Data from", data["aggregate_name"]
+        num_rows = data["num_values"];
+        rows_str = ""
 
-        ins_str_base = "INSERT INTO " + table_name + " values ('" + data["aggregate_id"] + "',"
+        col_types=[]
+        
+        for col_i in range(num_cols):
+            col_types.append(table_schema[col_i][1])
 
-        num_values = data["num_values"];
-        for i in range(num_values):
-            cur.execute(ins_str_base + str(data["times"][i]) + ", " + str(data["values"][i]) + ");")
+        for row_i in range(num_rows):
+            rows_str = rows_str + "("
+            for col_i in range(num_cols-1): 
+                # make the following a function
+                if col_types[col_i] == 'varchar':
+                    rows_str = rows_str + "'" + str(data[table_schema[col_i][0]][row_i]) + "', "
+                else: # TODO float, int ...
+                    rows_str = rows_str + str(data[table_schema[col_i][0]][row_i]) + ", "      
+    
+            # make the following a function
+            if col_types[col_i] == 'varchar':
+                rows_str = rows_str + "'" + str(data[table_schema[-1][0]][row_i]) + "')" 
+            else: # TODO float, int ...
+                rows_str = rows_str + str(data[table_schema[-1][0]][row_i]) + ")"
+
+            if (row_i < num_rows - 1):
+                rows_str = rows_str + ","
+
+        cur.execute(ins_str_base + rows_str + ";");
          
         con.commit()
             
 def json_fetcher():
 
-    payload = {'since':2}
+    payload = {'since':1390950580.92}
     r = requests.get('http://127.0.0.1:5000/memory_util/',params=payload)
     return r.content
 
@@ -48,7 +81,7 @@ def main():
 
 
     cur.execute("select count(*) from memory_util;");
-    print cur.fetchone()[0]
+    print 'Count is', cur.fetchone()[0]
 
 
 
