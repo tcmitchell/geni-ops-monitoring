528e89c462854bd76a31c578e04bf02ece2564b9
Ryan Irwin <rirwin@bbn.com>
2014-01-29 14:42:09 -0500

    Added auto schema to json receiver.  refactored code a little.

--- aggregator/py-progs/json_receiver.py
+++ aggregator/py-progs/json_receiver.py
@@ -2,6 +2,64 @@
 
 import json
 
+import sys
+sys.path.append("../../config")
+import db_schema_config
+full_schema = db_schema_config.get_schema()
+
+def get_psql_entry(col_type, value):
+
+    if col_type == 'varchar':
+        entry = "'" + str(value) + "'"
+    else: # TODO float, int if necessary
+        entry = str(value)
+
+    return entry
+
+def json_to_psql_auto_schema(json_text, table_str, thread_name):
+   
+    data = json.loads(json_text)
+    latest_time = 0
+    rows = []
+
+    if (data["response_type"] == "data_poll"):
+        table_name = data["data_type"]
+ 
+        if (table_name != table_str):
+            error_str = "Table name returned " + table_name + " but asked for " + table_str
+            return (-1, error_str, None)
+
+        table_schema = full_schema[table_name]
+        num_cols = len(table_schema)
+       
+        num_rows = data["num_values"];
+
+        col_types=[]
+        
+        for col_i in range(num_cols):
+            col_types.append(table_schema[col_i][1])
+
+        for row_i in range(num_rows):
+            
+            #row_string for eventual psql insert
+            row_str = "("
+            for col_i in range(num_cols): 
+                
+                row_str = row_str + get_psql_entry(col_types[col_i], data[table_schema[col_i][0]][row_i]) + ","
+
+                if table_schema[col_i][0] == 'time':
+                    if data['time'][row_i] > latest_time:
+                        latest_time = data['time'][row_i]
+
+            # remove last comma, add parens
+            row_str = row_str[:-1] + ")"
+
+            # append to rows array
+            rows.append(row_str)
+    
+    
+    return (0,rows,latest_time)
+
 #
 # Again this is very hard coded... :( need to load the schema into here for automatic decoding
 # it can be done
@@ -19,26 +77,27 @@ def json_to_psql(json_text, table_str, thread_name):
         else:
             num_values = data["num_values"]
             for i in range(num_values):
-                row = "('" + data["aggregate_ids"][i] + "','" + data["resource_ids"][i] + "'," + str(data["times"][i]) + ", " + str(data["values"][i]) + ")"
+                row = "('" + data["aggregate_id"][i] + "','" + data["resource_id"][i] + "'," + str(data["time"][i]) + ", " + str(data["value"][i]) + ")"
                 rows.append(row)
-                if data["times"][i] > latest_time:
-                    latest_time = data["times"][i]
+                if data["time"][i] > latest_time:
+                    latest_time = data["time"][i]
 
             return (0,rows,latest_time)
             
 
 def main():
-    json_text = '{"data_type": "memory_util", "num_values": 5, "times": [1390939480.98, 1390939481.08, 1390939481.19, 1390939481.29, 1390939481.39], "values": [32.6, 32.6, 32.6, 32.6, 32.6], "response_type": "data_poll", "aggregate_ids": ["404-ig", "404-ig", "404-ig", "404-ig", "404-ig"], "resource_ids": ["compute_node_1", "compute_node_1", "compute_node_1", "compute_node_1", "compute_node_1"]}'
+    json_text = '{"data_type": "memory_util", "num_values": 5, "time": [1390939480.98, 1390939481.08, 1390939481.19, 1390939481.29, 1390939481.39], "value": [32.6, 32.6, 32.6, 32.6, 32.6], "response_type": "data_poll", "aggregate_id": ["404-ig", "404-ig", "404-ig", "404-ig", "404-ig"], "resource_id": ["compute_node_1", "compute_node_1", "compute_node_1", "compute_node_1", "compute_node_1"]}'
     #print json_text
 
     table_str = "memory_util"
     thread_name = "thread-test"
     
-    (r_code, rows, latest_time) = json_to_psql(json_text, table_str, thread_name)
+    (r_code, rows, latest_time) = json_to_psql_auto_schema(json_text, table_str, thread_name)
     print r_code
+    print 'all rows', rows
     print rows[0]
     print rows[1]
-    print latest_time
+    print 'latest time =',latest_time
     
 if __name__ == "__main__":
     main()
