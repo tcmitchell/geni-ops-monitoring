15d5a14daec2db1859c5b70cbed8ca32a2bad785
Ryan Irwin <rirwin@bbn.com>
2014-01-29 18:07:07 -0500

    made this a class instead of a bunch of functions.  developed the opinion that each thing that populates a local store could have its own thread.  Not sure if this is the best idea, but this is a prototype of a peripheral that is not dicated by the architecture.

--- local/py-progs/local_store_memory_populator.py
+++ local/py-progs/local_store_memory_populator.py
@@ -8,54 +8,82 @@ import sys
 sys.path.append("../../config")
 import db_schema_config
 
-db_schema = db_schema_config.get_schema_dict()
-# 404 is not found and the area code in atlanta #TheGoalIsGEC19
-aggregate_id="404-ig" 
-resource_id="compute_node_1"
+# TODO add locking from threading in case multiple LocalStorePopulators
 
+# TODO make class LocalStorePopulator for this class to inherit
+# Intent is for each table to have its own object getting data
+class LocalStoreMemoryPopulator:
+    def __init__(self, con, aggregate_id, resource_id, num_inserts, sleep_period_sec, table_str):
+        self.con = con
+        self.aggregate_id = aggregate_id
+        self.resource_id = resource_id
+        self.num_inserts = num_inserts
+        self.sleep_period_sec = sleep_period_sec
+        self.table_str = table_str
+        self.table_schema = db_schema_config.get_schema_for_type(table_str)
 
-def get_mem_util_percent():
-    # despite being called virtual, this is physical memory
-    mem = psutil.virtual_memory() 
-    return mem.percent
+    def translate_psql_table_schema(self):
+        
+        schema_str = "("
+        for col_i in range(len(self.table_schema)):
+            schema_str = schema_str + self.table_schema[col_i][0] + " " + self.table_schema[col_i][1] + "," 
+        
+        # remove , and add ), makes a smug face [:-1]
+        return schema_str[:-1] + ")"
 
-def run_inserts(con, num_ins, per_sec):
-    
-    cur = con.cursor()
-    
-    for i in range(num_ins):
+    def run_inserts(self):
 
-        time_sec_epoch = time.time()
-        percent_mem_used = get_mem_util_percent()
+        for i in range(self.num_inserts):
 
-        ins_str = "INSERT INTO memory_util VALUES ('" + aggregate_id + "', '" + resource_id + "'," + str(time_sec_epoch) + "," + str(percent_mem_used) + ");" 
+            time_sec_epoch = time.time()
+            percent_mem_used = self.get_mem_util_percent()
 
-        cur.execute(ins_str)
-        con.commit()
+            ins_str = "INSERT INTO memory_util VALUES ('" + self.aggregate_id + "', '" + self.resource_id + "'," + str(time_sec_epoch) + "," + str(percent_mem_used) + ");" 
+            cur = self.con.cursor()            
+            cur.execute(ins_str)
+            self.con.commit()
+            cur.close()
 
-        time.sleep(per_sec)    
+            time.sleep(self.sleep_period_sec)    
 
-    cur.close()
+    def establish_table(self):
+        
+        # class functions should not be called in constructor
+        table_schema_str = self.translate_psql_table_schema();
 
+        # should this be in try catch?
+        cur = self.con.cursor()
+        
+        try:
+            cur.execute("create table if not exists " + self.table_str + " " + table_schema_str);
+            self.con.commit(); 
+        except psycopg2.Error as e:
+            print e
+        
+        cur = self.con.cursor()
 
-def main():
-    if (len(sys.argv) != 3):
+    def get_mem_util_percent(self):
+        # despite being called virtual, this is physical memory
+        mem = psutil.virtual_memory() 
+        return mem.percent
+
+def arg_parser(argv):
+    if (len(argv) != 3):
         print "Provide exactly two args: (1) for num inserts, (2) for period between inserts in seconds"
         sys.exit(1)
 
     num_ins = 0
 
     try:
-        num_ins = int(sys.argv[1])
+        num_ins = int(argv[1])
     except ValueError:
         print "Not a postive int. Provide number of inserts for run of program."
         sys.exit(1)
 
-
     per_sec = 0
 
     try:
-        per_sec = float(sys.argv[2])
+        per_sec = float(argv[2])
     except ValueError:
         print "Not a postive float. Provide period of time in seconds between inserts."
         sys.exit(1)
@@ -64,20 +92,27 @@ def main():
         print "Both numeric args should be positive"
         sys.exit(1)
 
-    con = psycopg2.connect("dbname=local user=rirwin");
-    cur = con.cursor();
+    return num_ins, per_sec
+
+def main():
+
+    # 404 is not found and the area code in atlanta #TheGoalIsGEC19
+    aggregate_id="404-ig" 
+    resource_id="compute_node_1"
     table_name = "memory_util"
 
-    cur.execute("drop table if exists " + table_name + ";");
-    con.commit(); 
-   
-    cur.execute("create table if not exists " + table_name + db_schema[table_name]);
-    con.commit(); 
+    (num_ins, per_sec) = arg_parser(sys.argv)
 
-    run_inserts(con, num_ins, per_sec)
+    con = psycopg2.connect("dbname=local user=rirwin");
 
+    lsmp = LocalStoreMemoryPopulator(con, aggregate_id, resource_id, num_ins, per_sec, table_name)
+    lsmp.establish_table()
+    lsmp.run_inserts()
+        
+
+    cur = con.cursor();
     cur.execute("select count(*) from memory_util;");
-    print cur.fetchone()[0]
+    print "num entries", cur.fetchone()[0]
 
     cur.close();
     con.close();
