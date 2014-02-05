911eedae97d80a9e2a17dc4687997e56996430ff
Ryan Irwin <rirwin@bbn.com>
2014-02-05 18:31:35 -0500

    updated how it receives the db schema

--- local/local_store_memory_populator.py
+++ local/local_store_memory_populator.py
@@ -16,15 +16,15 @@ import table_manager as TableManager
 # TODO make class LocalStorePopulator for this class to inherit
 # Intent is for each table to have its own object getting data
 class LocalStoreMemoryPopulator:
-    def __init__(self, con, aggregate_id, resource_id, num_inserts, sleep_period_sec, schema_dict, table_str):
+    def __init__(self, con, aggregate_id, resource_id, num_inserts, sleep_period_sec, db_templates, event_types, table_str):
         self.con = con;
         self.aggregate_id = aggregate_id
         self.resource_id = resource_id
         self.num_inserts = num_inserts
         self.sleep_period_sec = sleep_period_sec
-        self.table_schema = schema_dict[table_str]
+        #        self.table_schema = schema_dict[table_str]
         self.table_str = table_str
-        self.table_manager = TableManager.TableManager(self.con, schema_dict)
+        self.table_manager = TableManager.TableManager(self.con, db_templates, event_types)
         self.table_manager.establish_table(table_str)
 
     def run_inserts(self):
@@ -33,7 +33,7 @@ class LocalStoreMemoryPopulator:
             time_sec_epoch = int(time.time()*1000000)
             percent_mem_used = get_mem_util_percent()
 
-            ins_str = "INSERT INTO memory_util VALUES ('" + self.aggregate_id + "', '" + self.resource_id + "'," + str(time_sec_epoch) + "," + str(percent_mem_used) + ");" 
+            ins_str = "INSERT INTO " + self.table_str + " VALUES ('" + self.aggregate_id + "', '" + self.resource_id + "'," + str(time_sec_epoch) + "," + str(percent_mem_used) + ");" 
             cur = self.con.cursor()            
             cur.execute(ins_str)
             self.con.commit()
@@ -44,7 +44,7 @@ class LocalStoreMemoryPopulator:
 def get_mem_util_percent():
     # despite being called virtual, this is physical memory
     mem = psutil.virtual_memory() 
-    return mem.percent
+    return mem.used
 
 def arg_parser(argv):
     if (len(argv) != 3):
@@ -78,24 +78,23 @@ def main():
     # 404 is not found and the area code in atlanta #TheGoalIsGEC19
     aggregate_id="404-ig" 
     resource_id="compute_node_1"
-    table_str = "memory_util"
+    table_str = "mem_used"
 
-    schema_file = "../config/test_schema_dict"
-    json_data = open(schema_file)
-    schema_dict = json.load(json_data)
+    db_templates = json.load(open("../config/db_templates"))
+    event_types = json.load(open("../config/event_types"))
 
     (num_ins, per_sec) = arg_parser(sys.argv)
 
     db_con_str = "dbname=local user=rirwin";
     con = psycopg2.connect(db_con_str);
-    lsmp = LocalStoreMemoryPopulator(con, aggregate_id, resource_id, num_ins, per_sec, schema_dict, table_str)
+    lsmp = LocalStoreMemoryPopulator(con, aggregate_id, resource_id, num_ins, per_sec, db_templates, event_types, table_str)
 
     #lsmp.establish_table()
     lsmp.run_inserts()
         
     
     cur = con.cursor();
-    cur.execute("select count(*) from memory_util;");
+    cur.execute("select count(*) from " + table_str);
     print "num entries", cur.fetchone()[0]
 
     cur.close();
