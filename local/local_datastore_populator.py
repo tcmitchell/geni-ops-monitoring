a2197d2b9f5884adbe4b903cdb67d8db09544595
Ryan Irwin <rirwin@bbn.com>
2014-02-11 09:30:07 -0500

    Changed this class to be a base class for others to inherit. I need to make the local datastore populators easier to standup for testing and possibly deployment.

--- local/local_datastore_populator.py
+++ local/local_datastore_populator.py
@@ -5,217 +5,33 @@ import time
 import psutil
 import sys
 import json
+import threading
 from pprint import pprint as pprint
-
-#todo threading similar to single noun fetcher
 
 sys.path.append("../common/")
 import table_manager
 
-# TODO add locking from threading in case multiple LocalStorePopulators
-
-# TODO make class LocalStorePopulator for this class to inherit
-# Intent is for each table to have its own object getting data
 class LocalDatastorePopulator:
-    def __init__(self, con, aggregate_id, node_id, num_inserts, sleep_period_sec, table_manager, data_table_str_arr, info_table_str_arr):
-        self.con = con;
-        self.aggregate_id = aggregate_id
-        self.node_id = node_id
-        self.num_inserts = num_inserts
-        self.sleep_period_sec = sleep_period_sec
-        self.data_table_str_arr = data_table_str_arr
-        self.info_table_str_arr = info_table_str_arr 
+    def __init__(self, con, psql_lock,  table_manager):
+        self.con = con
+        self.psql_lock = psql_lock
         self.table_manager = table_manager
-
-        self.table_manager.establish_tables(data_table_str_arr)
-        self.table_manager.establish_tables(info_table_str_arr) 
-   
-    def run_node_inserts(self, table_str):
-
-        for i in range(self.num_inserts):
-            time_sec_epoch = int(time.time()*1000000)
-            percent_mem_used = get_data(table_str)
-
-            ins_str = "INSERT INTO " + table_str + " VALUES ('" + self.node_id + "'," + str(time_sec_epoch) + "," + str(percent_mem_used) + ");" 
-            cur = self.con.cursor()            
-            cur.execute(ins_str)
-            self.con.commit()
-            cur.close()
-
-            time.sleep(self.sleep_period_sec)    
-
-def get_data(table_str): 
-        if table_str == "mem_used":
-            return get_mem_util()
-
-def get_mem_util():
-    # despite being called virtual, this is physical memory
-    mem = psutil.virtual_memory() 
-    return mem.used
-
-def arg_parser(argv):
-
-    if (len(argv) != 3):
-        print "Provide exactly two args: (1) for num inserts, (2) for period between inserts in seconds"
-        sys.exit(1)
-
-    num_ins = 0
-
-    try:
-        num_ins = int(argv[1])
-    except ValueError:
-        print "Not a postive int. Provide number of inserts for run of program."
-        sys.exit(1)
-
-    per_sec = 0
-
-    try:
-        per_sec = float(argv[2])
-    except ValueError:
-        print "Not a postive float. Provide period of time in seconds between inserts."
-        sys.exit(1)
-
-    if (per_sec < 0 or num_ins < 0):
-        print "Both numeric args should be positive"
-        sys.exit(1)
-
-    return num_ins, per_sec
-
-def info_insert(con, table_str, row_arr):
-    val_str = "'"
-    for val in row_arr:
-        val_str += val + "','" # join won't do this
-    val_str = val_str[:-2] # remove last 2 of ','
-    cur = con.cursor()
-    print val_str
-    cur.execute("insert into " + table_str + " values (" + val_str + ")")
-    con.commit()
-    cur.close()
-
-
-def generate_and_insert_config_data(data_schema, info_schema):
-    # 404 is not found and the area code in atlanta #TheGoalIsGEC19
-    aggregate_id="404-ig" 
-    node_id="404-ig-pc1"
-    table_str = "mem_used"
-
-    data_table_str_arr = [table_str]
-    info_table_str_arr = ["aggregate","node","interface"]
-
-    # For table manager    
-    db_con_str = "dbname=local user=rirwin";
-    con = psycopg2.connect(db_con_str);
-
-    tm = table_manager.TableManager(con, data_schema, info_schema)
-    tm.drop_tables(data_schema.keys())
-    tm.drop_tables(info_schema.keys())
-    tm.establish_tables(data_schema.keys())
-    tm.establish_tables(info_schema.keys())
-    # End table manager
-
-    # Info about this local datastore populator
-    url_local_info = "http://127.0.0.1:5000/info/"
-    url_local_data = "http://127.0.0.1:5000/data/"
-
-    agg1 = []
-    agg1.append("http://www.gpolab.bbn.com/monitoring/schema/20140131/aggregate#")
-    agg1.append("404-ig")
-    agg1.append(url_local_info + "aggregate/" + agg1[1])
-    agg1.append("urn=404-ig+urn")
-    agg1.append(str(int(time.time()*1000000)))
-    agg1.append(url_local_data)
-
-    node1 = []
-    node1.append("http://unis.incntre.iu.edu/schema/20120709/node#")
-    node1.append("404-ig-pc1")
-    node1.append(url_local_info + "node/" + node1[1])
-    node1.append("urn=404-ig+pc1+urn")
-    node1.append(str(int(time.time()*1000000)))
-    node1.append(str(2*1000000)) # mem_total_kb
-
-    sliver1 = []
-    sliver1.append("http://www.gpolab.bbn.com/monitoring/schema/20140131/sliver#")
-    sliver1.append("404-ig-slv1")
-    sliver1.append(url_local_info + "sliver/" + sliver1[1])
-    sliver1.append("urn=404-ig+slv1+urn")
-    sliver1.append(str(int(time.time()*1000000)))
-
-    interface1 = []
-    interface1.append("http://unis.incntre.iu.edu/schema/20120709/port#")
-    interface1.append("404-ig-pc1:eth0")
-    interface1.append(url_local_info + "interface/" + interface1[1])
-    interface1.append("urn=404-ig+pc1+eth0+urn")
-    interface1.append(str(int(time.time()*1000000)))
-    interface1.append("ipv4")
-    interface1.append("192.1.242.140")
-    interface1.append("control")
-    interface1.append(str(10000000)) #max bps
-    interface1.append(str(1000000)) #max pps
-
-    aggres1 = []
-    aggres1.append("404-ig-pc1")
-    aggres1.append("404-ig")
-    aggres1.append("http://gpolab.bbn.com/404-ig-pc1")
-    aggres1.append(url_local_info + "node/" + aggres1[0])
-
-    aggsliv1 = []
-    aggsliv1.append("404-ig-slv1")
-    aggsliv1.append("404-ig")
-    aggsliv1.append("http://gpolab.bbn.com/sliver1:404-ig-slv1")
-    aggsliv1.append(url_local_info + "sliver/" + aggsliv1[0])
-                 
-    nodeiface1 = []
-    nodeiface1.append("404-ig-pc1:eth0")
-    nodeiface1.append("404-ig-pc1")
-    nodeiface1.append("http://gpolab.bbn.com/sliver1:404-ig-pc1")
-    nodeiface1.append(url_local_info + "interface/" + nodeiface1[0])
-
-    info_insert(con, "aggregate", agg1)
-    info_insert(con, "node", node1)
-    info_insert(con, "sliver", sliver1)
-    info_insert(con, "interface", interface1)
-    info_insert(con, "aggregate_resource",aggres1)
-    info_insert(con, "node_interface", nodeiface1)
-    info_insert(con, "aggregate_sliver", aggsliv1)
+       
 
 def main():
-
-    (num_ins, per_sec) = arg_parser(sys.argv)
-
-    # 404 is not found and the area code in atlanta #TheGoalIsGEC19
-    aggregate_id="404-ig" 
-    node_id="404-ig-pc1"
-    table_str = "mem_used"
-
-    data_table_str_arr = [table_str]
-    info_table_str_arr = ["aggregate","node","interface"]
 
     db_con_str = "dbname=local user=rirwin";
     con = psycopg2.connect(db_con_str);
     data_schema = json.load(open("../config/data_schema"))
     info_schema = json.load(open("../config/info_schema"))
+    
+    psql_lock = threading.Lock() 
 
+    # TODO consider passing psql_lock to table_manager
     tm = table_manager.TableManager(con, data_schema, info_schema)
 
-    generate_and_insert_config_data(data_schema, info_schema)
-
-    ldp = LocalDatastorePopulator(con, aggregate_id, node_id, num_ins, per_sec, tm, data_table_str_arr, info_table_str_arr)
+    ldp = LocalDatastorePopulator(con, psql_lock, tm)
     
-
-   
-
-
-    #
-    # TODO pass a dictionary and have it iterate through keys and pass
-    # to this function
-
-    ldp.run_node_inserts("mem_used")
-        
-    
-    cur = con.cursor();
-    cur.execute("select count(*) from " + table_str);
-    print "num entries", cur.fetchone()[0]
-
     cur.close();
     con.close();
 
