921a25356c2c8b5760b560d8163f0ae84821d536
Ryan Irwin <rirwin@bbn.com>
2014-02-11 13:44:57 -0500

    nicer unit test.  will extend to cover more events soon.

--- local/unit-test/local_config_restart_mem_populate.py
+++ local/unit-test/local_config_restart_mem_populate.py
@@ -1,38 +1,63 @@
 import sys
 import json
 import psycopg2
+import threading
 
 sys.path.append("../../common/")
 import table_manager
 sys.path.append("../")
 import local_datastore_populator
+import info_populator
+import node_stats_populator
 
-info_schema = json.load(open("../../config/info_schema"))
-data_schema = json.load(open("../../config/data_schema"))
+def main():
+    info_schema = json.load(open("../../config/info_schema"))
+    data_schema = json.load(open("../../config/data_schema"))
 
-table_str_arr = info_schema.keys() + data_schema.keys()
+    table_str_arr = info_schema.keys() + data_schema.keys()
 
-db_con_str = "dbname=local user=rirwin"
-con = psycopg2.connect(db_con_str);
- 
-tm = table_manager.TableManager(con, data_schema, info_schema)
+    db_con_str = "dbname=local user=rirwin"
+    con = psycopg2.connect(db_con_str);
+    
+    tbl_mgr = table_manager.TableManager(con, data_schema, info_schema)
 
-tm.drop_tables(table_str_arr)
-tm.establish_tables(table_str_arr)
+    tbl_mgr.drop_tables(table_str_arr)
+    tbl_mgr.establish_tables(table_str_arr)
+    node_id = "404-ig-pc1"
+    
+    num_ins = 10
+    per_sec = 0.2
 
-sys.argv.append(10)
-sys.argv.append(0.2)
+    psql_lock = threading.Lock()
 
-local_datastore_populator.generate_and_insert_config_data(data_schema, info_schema)   
+    # local datastore populator helper
+    ldph = local_datastore_populator.LocalDatastorePopulator(con, psql_lock, tbl_mgr)
 
-# 404 is not found and the area code in atlanta #TheGoalIsGEC19
-aggregate_id="404-ig" 
-node_id="404-ig-pc1"
-table_str = "mem_used"    
-data_table_str_arr = [table_str]
-info_table_str_arr = ["aggregate","node","interface"]
-num_ins = 10
-per_sec = 0.2
-ldp = local_datastore_populator.LocalDatastorePopulator(con, aggregate_id, node_id, num_ins, per_sec, tm, data_table_str_arr, info_table_str_arr)
+    # info population
+    ip = info_populator.InfoPopulator(ldph)
 
-ldp.run_node_inserts("mem_used")
+    ip.ins_fake_info()
+       
+    cur = con.cursor();
+    cur.execute("select count(*) from aggregate");
+    print "Aggregate has entries", cur.fetchone()[0], "entries"
+    
+    # data population
+    event_str_arr = []
+    #event_str_arr = data_schema.keys()
+
+    event_str_arr.append("mem_used")
+    
+    nsp = node_stats_populator.NodeStatsPopulator(ldph, node_id, num_ins, per_sec, event_str_arr)
+
+    nsp.run_node_stats_main()
+
+    for ev in event_str_arr:
+        cur.execute("select count(*) from " + ev);
+        print ev, "has", cur.fetchone()[0], "entries"
+
+    cur.close()
+    con.close()
+
+if __name__ == "__main__":
+    main()
