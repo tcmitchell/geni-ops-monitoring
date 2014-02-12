755dc7362f3c1d7db526b420b5c0f051daa57102
Ryan Irwin <rirwin@bbn.com>
2014-02-12 16:09:36 -0500

    No need for json producer any more.  The flask webserver is down to 37 lines of code. The query handler now handles all rest calls

--- local/query_handler.py
+++ local/query_handler.py
@@ -3,6 +3,164 @@ from pprint import pprint as pprint
 import json
 import psycopg2
 
+    # remove this comment once documented on wiki
+    # Valid call
+    #{"filters":{"eventType": ["mem_used","cpu_util"],"ts":{"gte":3,"lt":5},"obj":{"type":"node","id":["404-ig-pc1","404-ig-pc2"]}}}
+
+def check_data_query_keys(q_dict):
+
+    if "filters" not in q_dict:
+        return (False, "query: " + str(q_dict) + "<br><br>has dictionary error.  It is missing filters key")
+        
+    missing_keys = []
+    if "ts" not in q_dict["filters"]:
+        missing_keys.append("ts")
+    if "eventType" not in q_dict["filters"]:
+        missing_keys.append("eventType")
+    if "obj" not in q_dict["filters"]:
+        missing_keys.append("obj")
+
+    if len(missing_keys) > 0:
+        return (False, "query: " + str(q_dict) + "<br><br>has dictionary error.  It is missing keys: " + str(missing_keys))
+
+    return (True, None)
+
+def handle_ts_data_query(con, filters, schema_dict):
+
+    # try to make a dictionary
+    try:
+        q_dict = eval(filters) 
+
+    except Exception, e:
+        return "query: " + filters + "<br><br>had error: " + str(e) + "<br><br> failed to evaluate as dictionary"
+
+    # check for necessary keys
+    (ok, fail_str) = check_data_query_keys(q_dict)
+    if ok == True:
+        ts_filters = q_dict["filters"]["ts"]
+        event_types = q_dict["filters"]["eventType"]
+        objects = q_dict["filters"]["obj"]
+    else:
+        return fail_str # returns why filters failed
+
+    ts_where_str = build_ts_where_str(ts_filters)
+    resp_arr = []    
+    
+    for event_type in event_types:
+        obj_type = objects["type"]
+        for obj_id in objects["id"]:
+            resp_i = {}
+            ts_arr = get_tsdata(con, event_type, obj_type, obj_id, ts_where_str)
+            if (ts_arr != None):
+                resp_i["$schema"] = "http://www.gpolab.bbn.com/monitoring/schema/20140131/data#"
+                resp_i["id"] = obj_id + ":" + event_type
+                resp_i["subject"] = obj_id
+                resp_i["eventType"] = event_type
+                resp_i["description"] = event_type + " for " + obj_id + " of type " + obj_type
+                resp_i["units"] = schema_dict["units"][event_type]
+                resp_i["tsdata"] = ts_arr
+                resp_arr.append(resp_i)
+
+    return json.dumps(resp_arr)
+ 
+def get_interface_info_dict(schema, info_row):
+
+    json_dict = {}
+    for col_i in range(len(schema)):
+        if schema[col_i][0] == "address_address":
+            addr = info_row[col_i]
+        elif schema[col_i][0] == "address_type":
+            addr_type = info_row[col_i]
+        else:
+            json_dict[schema[col_i][0]] = info_row[col_i]
+            
+    json_dict["address"] = {"address":addr,"type":addr_type}
+
+    return json_dict
+
+
+def handle_iface_info_query(con, iface_id, iface_schema):
+    table_str = "interface"
+    iface_info = get_object_info(con, table_str, iface_id)
+
+    if iface_info != None:
+        return json.dumps(get_interface_info_dict(iface_schema, iface_info))
+    else:
+        return "port not found"
+
+
+def get_node_info_dict(node_id, schema, info_row, port_refs =[]):
+
+    json_dict = {}
+    for col_i in range(len(schema)):
+        json_dict[schema[col_i][0]] = info_row[col_i]
+            
+    if len(port_refs) > 0 and port_refs[0] != None:
+        json_dict["ports"] = []
+        for port_ref in port_refs:
+            json_dict["ports"].append({"href":port_ref[0],"urn":port_ref[1]})
+            
+    return json_dict
+
+def handle_node_info_query(con, node_id, node_schema):
+    
+    table_str = "node"
+    iface_refs = []
+
+    node_info = get_object_info(con, table_str, node_id)
+
+    if node_info != None:
+
+        ifaces = get_node_interfaces(con, node_id)
+        for iface_id in ifaces:
+            iface_refs.append(get_refs(con, "interface", iface_id))
+
+        return json.dumps(get_node_info_dict(node_id, node_schema, node_info, iface_refs))
+
+    else:
+        return "resource not found"
+
+
+def get_agg_info_dict(agg_id, info_row, schema, res_refs =[], slv_refs = []):
+
+    json_dict = {}
+    for col_i in range(len(schema)):
+        json_dict[schema[col_i][0]] = info_row[col_i]
+            
+    if len(res_refs) > 0 and res_refs[0] != None:
+        json_dict["resources"] = []
+        for res_ref in res_refs:
+            json_dict["resources"].append({"href":res_ref[0],"urn":res_ref[1]})
+
+    if len(slv_refs) > 0 and slv_refs[0] != None:
+        json_dict["slivers"] = []
+        for slv_ref in slv_refs:
+            json_dict["slivers"].append({"href":slv_ref[0],"urn":slv_ref[1]})  
+    return json_dict
+
+
+def handle_agg_info_query(con, agg_id, agg_schema):
+    table_str = "aggregate"
+    res_refs = []    
+    slv_refs = []
+
+    agg_info = get_object_info(con, table_str, agg_id)
+    if agg_info != None:
+
+        resources = get_agg_nodes(con, agg_id)
+        for res_id in resources:
+            res_refs.append(get_refs(con, "resource", res_id))
+
+        slivers = get_agg_slivers(con, agg_id)
+        print "slivers",slivers
+        for slv_id in slivers:
+            slv_refs.append(get_refs(con, "sliver", slv_id))
+
+        return json.dumps(get_agg_info_dict(agg_id, agg_info, agg_schema, res_refs, slv_refs))
+
+    else:
+        return "aggregate not found"
+
 def select_distinct_query(con, table, distinct_filter = "",  where_filter=""):
     cur = con.cursor()
     print "cursor open"
@@ -90,7 +248,6 @@ def get_object_info(con, table_str, obj_id):
     try:
         cur.execute("select * from " + table_str + " where id = '" + obj_id + "' order by ts desc limit 1")
         res = cur.fetchone()
-       
     except Exception, e:
         print e
     
@@ -142,30 +299,6 @@ def build_ts_where_str(ts_dict):
         print str(e), "ts filters has invalid key or value:"
 
     return ts_where_str
-
-# handles query and json response
-def handle_data_query(con, event_types, objects, ts_filters, schema_dict):
-    resp_arr = []
-    
-    ts_where_str = build_ts_where_str(ts_filters)
-    
-    for event_type in event_types:
-        obj_type = objects["type"]
-        for obj_id in objects["id"]:
-            resp_i = {}
-            ts_arr = get_tsdata(con, event_type, obj_type, obj_id, ts_where_str)
-            if (ts_arr != None):
-                resp_i["$schema"] = "http://www.gpolab.bbn.com/monitoring/schema/20140131/data#"
-                resp_i["id"] = obj_id + ":" + event_type
-                resp_i["subject"] = obj_id
-                resp_i["eventType"] = event_type
-                resp_i["description"] = event_type + " for " + obj_id + " of type " + obj_type
-                resp_i["units"] = schema_dict["units"][event_type]
-                resp_i["tsdata"] = ts_arr
-                #pprint(resp_i)
-                resp_arr.append(resp_i)
-
-    return json.dumps(resp_arr)
 
 
 def get_tsdata(con, event_type, obj_type, obj_id, ts_where_str):
--- local/web_server.py
+++ local/web_server.py
@@ -10,130 +10,38 @@ from pprint import pprint as pprint
 sys.path.append("../common")
 import table_manager
 app = Flask(__name__)
-
-def check_data_query_keys(q_dict):
-    missing_keys = []
-
-    if "filters" not in q_dict:
-        missing_keys.append("filter")
-    if "ts" not in q_dict["filters"]:
-        missing_keys.append("ts")
-    if "eventType" not in q_dict["filters"]:
-        missing_keys.append("eventType")
-    if "obj" not in q_dict["filters"]:
-        missing_keys.append("obj")
-    if len(missing_keys) > 0:
-        return (False, "query: " + str(q_dict) + "<br><br>has dictionary error.  It is missing keys: " + str(missing_keys))
-    
-    return (True, None)
-        
-
-'''
-# fix json import
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
-'''
-
-@app.route('/', methods = ['GET'])
-def index():
-    return "Hello, World!"
-
-@app.route('/info', methods = ['GET'])
-def info():
-    return "info great"
-
-
+       
 @app.route('/info/aggregate/<agg_id>', methods = ['GET'])
 def info_aggregate_args(agg_id): 
-    print "info_agg_args(",agg_id,")"
-    table_str = "aggregate"
-    res_refs = []    
-    slv_refs = []
-    
-    agg_info = query_handler.get_object_info(con, table_str, agg_id)
-    if agg_info != None:
+    return query_handler.handle_agg_info_query(con, agg_id, tm.schema_dict["aggregate"])
 
-        resources = query_handler.get_agg_nodes(con, agg_id)
-        for res_id in resources:
-            res_refs.append(query_handler.get_refs(con, "resource", res_id))
-
-        slivers = query_handler.get_agg_slivers(con, agg_id)
-        print "slivers",slivers
-        for slv_id in slivers:
-            slv_refs.append(query_handler.get_refs(con, "sliver", slv_id))
-
-        return jp.json_agg_info(table_str, agg_info, res_refs, slv_refs)
-
-    else:
-        return "aggregate not found"
-
+   
 @app.route('/info/node/<node_id>', methods = ['GET'])
 def info_node_args(node_id): 
-    print "info_node_args(",node_id,")"
-    table_str = "node"
-    iface_refs = []
+    return query_handler.handle_node_info_query(con, node_id, tm.schema_dict["node"])
 
-    node_info = query_handler.get_object_info(con, table_str, node_id)
-    if node_info != None:
-
-        ifaces = query_handler.get_node_interfaces(con, node_id)
-        for iface_id in ifaces:
-            iface_refs.append(query_handler.get_refs(con, "interface", iface_id))
-
-        return jp.json_node_info(table_str, node_info, iface_refs)
-
-    else:
-        return "resource not found"
     
 @app.route('/info/interface/<iface_id>', methods = ['GET'])
-def info_interface_args(iface_id): # gets interface info
-    print "info_interface_args(",iface_id,")"
-    table_str = "interface"
-    iface_info = query_handler.get_object_info(con, table_str, iface_id)
-
-    if iface_info != None:
-        return jp.json_interface_info(iface_info)
-    else:
-        return "port not found"
+def info_interface_args(iface_id): 
+    return query_handler.handle_iface_info_query(con, iface_id, tm.schema_dict["interface"])
 
 
 @app.route('/data/', methods = ['GET'])
-def data_v2(): 
-    # get everything to the right of ?q= as string from Flask
-    # then stop using Flask
-    #
-    # Valid call
-    #
-    #{"filters":{"eventType": ["mem_used","cpu_util"],"ts":{"gte":3,"lt":5},"obj":{"type":"node","id":["404-ig-pc1","404-ig-pc2"]}}}
-    q = request.args.get('q', None)
-    ret_str = ""
-    try:
-        q_dict = eval(q)
-        (ok, fail_str) = check_data_query_keys(q_dict)
-        if ok == True:
-        
-            ts_filters = q_dict["filters"]["ts"]
-            event_types = q_dict["filters"]["eventType"]
-            objects = q_dict["filters"]["obj"]
-        else:
-            return fail_str
-    except Exception, e:
-        return "query: " + q + "<br><br>had error: " + str(e) + "<br><br> failed to evaluate as dictionary"       
-
-    ## do query handling and json production
-    return query_handler.handle_data_query(con, event_types, objects, ts_filters, tm.schema_dict)
+def data(): 
+    # get everything to the right of ?q= as string from flask.request
+    filters = request.args.get('q', None)
+    return query_handler.handle_ts_data_query(con, filters, tm.schema_dict)
+     
 
 
 if __name__ == '__main__':
 
     con = psycopg2.connect("dbname=local user=rirwin");
 
-    # Dense lines to get schema_dict
     info_schema = json.load(open("../config/info_schema"))
     data_schema = json.load(open("../config/data_schema"))
 
     tm = table_manager.TableManager(con, data_schema, info_schema)
-    jp = json_producer.JsonProducer(tm.schema_dict);
+
 
     app.run(debug = True)
