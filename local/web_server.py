9f55482c5a3e98fa279c89af0ddae477efb5b5d1
Chaos Golubitsky <chaos@bbn.com>
2014-02-19 16:49:48 -0500

    * move the app functionality into a class, so we can invoke it with an   argument of the parent directory location, and get that path out of   the code

--- local/web_server.py
+++ local/web_server.py
@@ -1,57 +1,67 @@
-#!flask/bin/python
 from flask import Flask, request
-
-import rest_call_handler
 import sys
 
-common_path = "../common/"
-sys.path.append(common_path)
-import table_manager
+class LocalDatastoreServer:
+    def __init__(self, parent_path):
+        self.local_path = parent_path + "/local/"
+        self.common_path = parent_path + "/common/"
+        self.config_path = parent_path + "/config/"
+        sys.path.append(self.local_path)
+        sys.path.append(self.common_path)
+        sys.path.append(self.config_path)
 
-db_name = "local"
-config_path = "../config/"
-tm = table_manager.TableManager(db_name, config_path)
+        import rest_call_handler
+        import table_manager
 
-app = Flask(__name__)
+        self.db_name = "local"
+        self.tm = table_manager.TableManager(self.db_name, self.config_path)
+
+        self.app = Flask(__name__)
        
-@app.route('/info/aggregate/<agg_id>', methods = ['GET'])
-def info_aggregate_args(agg_id): 
-    return rest_call_handler.handle_aggregate_info_query(tm, agg_id)
+        @self.app.route('/info/aggregate/<agg_id>', methods = ['GET'])
+        def info_aggregate_args(agg_id): 
+            return rest_call_handler.handle_aggregate_info_query(
+                self.tm, agg_id)
 
-@app.route('/info/node/<node_id>', methods = ['GET'])
-def info_node_args(node_id): 
-    return rest_call_handler.handle_node_info_query(tm, node_id)
-
-@app.route('/info/interface/<iface_id>', methods = ['GET'])
-def info_interface_args(iface_id): 
-    return rest_call_handler.handle_interface_info_query(tm, iface_id)
-
-@app.route('/info/sliver/<sliver_id>', methods = ['GET'])
-def info_sliver_args(sliver_id): 
-    return rest_call_handler.handle_sliver_info_query(tm, sliver_id)
-
-@app.route('/info/slice/<slice_id>', methods = ['GET'])
-def info_slice_args(slice_id): 
-    return rest_call_handler.handle_slice_info_query(tm, slice_id)
-
-@app.route('/info/user/<user_id>', methods = ['GET'])
-def info_user_args(user_id): 
-    return rest_call_handler.handle_user_info_query(tm, user_id)
-
-@app.route('/info/authority/<authority_id>', methods = ['GET'])
-def info_authority_args(authority_id): 
-    return rest_call_handler.handle_authority_info_query(tm, authority_id)
-
-@app.route('/info/opsconfig/<opsconfig_id>', methods = ['GET'])
-def info_opsconfig_args(opsconfig_id): 
-    return rest_call_handler.handle_opsconfig_info_query(tm, opsconfig_id)
-
-@app.route('/data/', methods = ['GET'])
-def data(): 
-    # get everything to the right of ?q= as string from flask.request
-    filters = request.args.get('q', None)
-    return rest_call_handler.handle_ts_data_query(tm, filters)
-
+        @self.app.route('/info/node/<node_id>', methods = ['GET'])
+        def info_node_args(node_id): 
+            return rest_call_handler.handle_node_info_query(self.tm, node_id)
+        
+        @self.app.route('/info/interface/<iface_id>', methods = ['GET'])
+        def info_interface_args(iface_id): 
+            return rest_call_handler.handle_interface_info_query(
+                self.tm, iface_id)
+        
+        @self.app.route('/info/sliver/<sliver_id>', methods = ['GET'])
+        def info_sliver_args(sliver_id): 
+            return rest_call_handler.handle_sliver_info_query(
+                self.tm, sliver_id)
+        
+        @self.app.route('/info/slice/<slice_id>', methods = ['GET'])
+        def info_slice_args(slice_id): 
+            return rest_call_handler.handle_slice_info_query(self.tm, slice_id)
+        
+        @self.app.route('/info/user/<user_id>', methods = ['GET'])
+        def info_user_args(user_id): 
+            return rest_call_handler.handle_user_info_query(self.tm, user_id)
+        
+        @self.app.route('/info/authority/<authority_id>', methods = ['GET'])
+        def info_authority_args(authority_id): 
+            return rest_call_handler.handle_authority_info_query(
+                self.tm, authority_id)
+        
+        @self.app.route('/info/opsconfig/<opsconfig_id>', methods = ['GET'])
+        def info_opsconfig_args(opsconfig_id): 
+            return rest_call_handler.handle_opsconfig_info_query(
+                self.tm, opsconfig_id)
+        
+        @self.app.route('/data/', methods = ['GET'])
+        def data(): 
+            # get everything to the right of ?q= as string from flask.request
+            filters = request.args.get('q', None)
+            return rest_call_handler.handle_ts_data_query(self.tm, filters)
+        
 if __name__ == '__main__':
 
-    app.run(debug = True)
+    server = LocalDatastoreServer('..')
+    server.app.run(debug = True)
