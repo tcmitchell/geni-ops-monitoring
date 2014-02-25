9ef1ce6bd9b2fb624bc2852c1b330958e54f094b
Ryan Irwin <rirwin@bbn.com>
2014-02-25 14:58:19 -0500

    Remove byte counters and added per second rates for bytes, packets total, errors, drops for rx and tx
    Added these to the stats populator and handle rate calculation
    
    And added to the unit test and works in the rest call handler

--- local/stats_populator.py
+++ local/stats_populator.py
@@ -36,6 +36,27 @@ common_path = "../common/"
 sys.path.append(common_path)
 import table_manager
 
+# global variables for interface rate calculations
+ts_last_rx_bps = int(time.time()*1000000)
+bytes_last_rx_bps = psutil.network_io_counters().bytes_recv
+ts_last_tx_bps = int(time.time()*1000000)
+bytes_last_tx_bps = psutil.network_io_counters().bytes_sent
+
+ts_last_rx_pps = int(time.time()*1000000)
+pkts_last_rx_pps = psutil.network_io_counters().packets_recv
+ts_last_tx_pps = int(time.time()*1000000)
+pkts_last_tx_pps = psutil.network_io_counters().packets_sent
+
+ts_last_rx_eps = int(time.time()*1000000)
+pkts_last_rx_eps = psutil.network_io_counters().errin
+ts_last_tx_eps = int(time.time()*1000000)
+pkts_last_tx_eps = psutil.network_io_counters().errout
+
+ts_last_rx_dps = int(time.time()*1000000)
+pkts_last_rx_dps = psutil.network_io_counters().dropin
+ts_last_tx_dps = int(time.time()*1000000)
+pkts_last_tx_dps = psutil.network_io_counters().dropout
+
 
 class StatsPopulator(threading.Thread):
     def __init__(self, tbl_mgr, obj_id, num_inserts, sleep_period_sec, event_types_arr):
@@ -74,7 +95,27 @@ class StatsPopulator(threading.Thread):
 
 # Simple calls to get data
 # These should be non-blocking
-def get_data(event_type):        
+def get_data(event_type):
+    
+    # global variables for handling rates, could add to class
+    global bytes_last_rx_bps
+    global bytes_last_tx_bps
+    global pkts_last_rx_pps
+    global pkts_last_tx_pps
+    global pkts_last_rx_eps
+    global pkts_last_tx_eps
+    global pkts_last_rx_dps
+    global pkts_last_tx_dps
+
+    global ts_last_rx_bps
+    global ts_last_tx_bps
+    global ts_last_rx_pps
+    global ts_last_tx_pps
+    global ts_last_rx_eps
+    global ts_last_tx_eps
+    global ts_last_rx_dps
+    global ts_last_tx_dps
+
     if event_type == "mem_used":
         return psutil.virtual_memory().used
     elif event_type == "swap_free":
@@ -83,10 +124,78 @@ def get_data(event_type):
         return psutil.cpu_percent(interval=0)
     elif event_type == "disk_part_max_used":
         return psutil.disk_usage('/').percent
-    elif event_type == "rx_bytes":
-        return psutil.network_io_counters().bytes_recv
-    elif event_type == "tx_bytes":
-        return psutil.network_io_counters().bytes_sent
+    elif event_type == "rx_bps":
+        prev_val = bytes_last_rx_bps
+        curr_val = psutil.network_io_counters().bytes_recv
+        prev_ts = ts_last_rx_bps
+        curr_ts = int(time.time()*1000000)
+        rx_bps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_rx_bps = curr_ts
+        bytes_last_rx_bps = curr_val
+        return max(0, rx_bps) # TODO handle rollover
+    elif event_type == "tx_bps":
+        prev_val = bytes_last_tx_bps
+        curr_val = psutil.network_io_counters().bytes_sent
+        prev_ts = ts_last_tx_bps
+        curr_ts = int(time.time()*1000000)
+        tx_bps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_tx_bps = curr_ts
+        bytes_last_tx_bps = curr_val
+        return max(0, tx_bps) # TODO handle rollover
+    elif event_type == "rx_pps":
+        prev_val = pkts_last_rx_pps
+        curr_val = psutil.network_io_counters().packets_recv
+        prev_ts = ts_last_rx_pps
+        curr_ts = int(time.time()*1000000)
+        rx_pps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_rx_pps = curr_ts
+        pkts_last_rx_pps = curr_val
+        return max(0, rx_pps) # TODO handle rollover
+    elif event_type == "tx_pps":
+        prev_val = pkts_last_tx_pps
+        curr_val = psutil.network_io_counters().packets_sent
+        prev_ts = ts_last_tx_pps
+        curr_ts = int(time.time()*1000000)
+        tx_pps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_tx_pps = curr_ts
+        pkts_last_tx_pps = curr_val
+        return max(0, tx_pps) # TODO handle rollover
+    elif event_type == "rx_eps":
+        prev_val = pkts_last_rx_eps
+        curr_val = psutil.network_io_counters().errin
+        prev_ts = ts_last_rx_eps
+        curr_ts = int(time.time()*1000000)
+        rx_eps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_rx_eps = curr_ts
+        pkts_last_rx_eps = curr_val
+        return max(0, rx_eps) # TODO handle rollover
+    elif event_type == "tx_eps":
+        prev_val = pkts_last_tx_eps
+        curr_val = psutil.network_io_counters().errout
+        prev_ts = ts_last_tx_eps
+        curr_ts = int(time.time()*1000000)
+        tx_eps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_tx_eps = curr_ts
+        pkts_last_tx_eps = curr_val
+        return max(0, tx_eps) # TODO handle rollover
+    elif event_type == "rx_dps":
+        prev_val = pkts_last_rx_dps
+        curr_val = psutil.network_io_counters().dropin
+        prev_ts = ts_last_rx_dps
+        curr_ts = int(time.time()*1000000)
+        rx_dps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_rx_dps = curr_ts
+        pkts_last_rx_dps = curr_val
+        return max(0, rx_dps) # TODO handle rollover
+    elif event_type == "tx_dps":
+        prev_val = pkts_last_tx_dps
+        curr_val = psutil.network_io_counters().dropout
+        prev_ts = ts_last_tx_dps
+        curr_ts = int(time.time()*1000000)
+        tx_dps = ((curr_val - prev_val)/(curr_ts - prev_ts))/1000000
+        ts_last_tx_dps = curr_ts
+        pkts_last_tx_dps = curr_val
+        return max(0, tx_dps) # TODO handle rollover
     else: # TODO add more
         return None
 
@@ -128,14 +237,19 @@ def main():
     db_name = "local"
     config_path = "../config/"
     tbl_mgr = table_manager.TableManager(db_name, config_path)
-    
+    data_schema = json.load(open(config_path + "data_schema"))
+    table_str_arr = data_schema.keys()
+    tbl_mgr.drop_tables(table_str_arr)
+    tbl_mgr.establish_tables(table_str_arr)
+
+
 
     node_id="instageni.gpolab.bbn.com_node_pc1"
     event_types_arr = ["mem_used","cpu_util","disk_part_max_used"]
     nsp = StatsPopulator(tbl_mgr, node_id, num_ins, per_sec, event_types_arr)
  
     iface_id="instageni.gpolab.bbn.com_interface_pc1:eth0"
-    event_types_arr = ["rx_bytes","tx_bytes"]
+    event_types_arr = ["rx_bps","tx_bps","rx_pps","tx_pps","rx_eps","tx_eps","rx_dps","tx_dps"]
     isp = StatsPopulator(tbl_mgr, iface_id, num_ins, per_sec, event_types_arr)
 
     nsp.start()
