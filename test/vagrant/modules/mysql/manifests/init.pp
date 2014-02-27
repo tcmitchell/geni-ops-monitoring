8a0c8c427411a0944cd0a4913f8d0000bb120db3
Chaos Golubitsky <chaos@bbn.com>
2014-02-27 14:37:39 -0500

    * code now reads what database type to use from the config file * README and vagrant now support a mysql-backed local datastore

---
+++ test/vagrant/modules/mysql/manifests/init.pp
@@ -0,0 +1,46 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+class mysql::server {
+  package {
+    "mysql-server": ensure => installed;
+    "mysql-client": ensure => installed;
+    "python-mysqldb": ensure => installed;
+  }
+
+  file {
+    "/usr/local/sbin/set_mysql_passwords":
+      content => template("mysql/set_passwords.erb"),
+      mode => 0555;
+  }
+
+  exec {
+    "mysql_set_passwords":
+      command => "/usr/local/sbin/set_mysql_passwords",
+      require => [
+        File["/usr/local/sbin/set_mysql_passwords"],
+        Package["mysql-server"],
+        Package["mysql-client"]
+      ];
+  }
+}
