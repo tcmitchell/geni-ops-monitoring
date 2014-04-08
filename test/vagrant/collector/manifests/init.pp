2dd6bf6bf737ea60732d6bd003662f6791fb1cfc
Ryan Irwin <rirwin@dhcp89-73-68.bbn.com>
2014-04-08 14:29:36 -0400

    Added a collector database config module similar to the local store database config module.
    Added a collector vagrant file and manifest
    
    Added an emacs module, bc vi is still not my best friend.

---
+++ test/vagrant/collector/manifests/init.pp
@@ -0,0 +1,60 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+node default {
+
+  ## Defaults
+
+  # Always run apt-get update before trying to install packages
+  Package {
+    require => Exec["apt_client_update"],
+  }
+
+  # mysql or postgresql
+  $database_type = "mysql"
+
+  # exactly one of these should be true
+  $populate_data = false
+  $populate_config_store = false
+  $init_collector = true
+
+  $database_name = "collector"
+  $config_store_url = "http://starkville.bbn.com/info/opsconfig/geni-prod"
+
+  # since these passwords are being committed to a repo, never use
+  # them anywhere outside a vagrant instance running on localhost
+  $postgres_superuser_password = "d86LJY278htqSkrP2oNx"
+  $postgres_localstore_password = "yz9nQxB9TbF74jmMQbXs"
+  $mysql_localstore_password = "JU63p3MBGjnUmzbv3apQ"
+
+  include "collector"
+}
+
+class collector {
+
+  include "apt::client"
+  include "emacs::base"
+  include "collector::server"
+  include "${database_type}::server"
+
+}
---
+++ test/vagrant/modules/collector/manifests/init.pp
@@ -0,0 +1,46 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+class collector::server {
+
+  $collector_setup_script = "collector_table_reset.py"
+
+  exec {
+    "update_code":
+      command => "/usr/bin/rsync -a --delete /ops-monitoring/ /usr/local/ops-monitoring/";
+
+    "local_setup":
+      command => "/usr/bin/python ./${collector_setup_script}",
+      cwd => "/usr/local/ops-monitoring/collector/unit-tests",
+      require => [
+        File["/usr/local/ops-monitoring/config/collector_operator.conf"],
+        Exec["${database_type}_set_passwords"]
+      ];
+  }
+
+  file {
+    "/usr/local/ops-monitoring/config/collector_operator.conf":
+      content => template("collector/collector_operator.conf.erb"),
+      require => Exec["update_code"];
+  }
+}
