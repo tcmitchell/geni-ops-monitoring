4d54502a5633eb60711c97a0c34d828ee18fc46f
Ryan Irwin <rirwin@dhcp89-73-68.bbn.com>
2014-05-06 18:01:24 -0400

    Added a program that allows iterative crawling of a datastore.
    It tests each response in against the json-schema with validictory
    
    It extracts all urls in the response and prompts the user for which
    url/REST call to try next.

---
+++ collector/unit-tests/multi_rest_call.py
@@ -0,0 +1,148 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+'''
+This program is designed to make it easier to debug a (local)datastore's 
+output.  If the datastore's database tables are populated properly, then
+everything will work fine.  This program enables debugging of this, since
+debugging in the web-browser is no longer possible with the client 
+certificate credential system.
+'''
+
+import json
+import sys
+import requests
+import getopt
+from pprint import pprint as pprint
+
+sys.path.append("../")
+import response_validator
+
+urls_in_resp = []
+
+def usage():
+    print "Correct usage is:"
+    print "python single_rest_call.py https://<datastore url>/info/<object_type>/<object_id>"
+    print "hard code the location of your cert in main() for easier execution"
+    sys.exit(-1)
+
+def make_request_print_response(url, cert_path):
+
+    resp = None
+    json_dict = None
+
+    try:
+        resp = requests.get(url, verify=False, cert=cert_path)
+    except Exception, e:
+        print "No response from local datastore at: " + url
+        print e
+
+    if resp:
+        try:
+            json_dict = json.loads(resp.content)
+        except Exception, e:
+            print "Could not load into JSON",
+            print resp.content
+            print e
+
+        pprint(json_dict)
+    else:
+        print "resp object is None from", url
+
+    return json_dict
+
+
+def recurse_urls(d):
+
+    global urls_in_resp
+
+    if "href" in d:
+        urls_in_resp.append(d["href"])
+
+    if "selfRef" in d:
+        urls_in_resp.append(d["selfRef"])
+
+    if type(d)==type({}):
+        for k in d:
+            if type(d[k])==type({}):
+                recurse_urls(d[k])
+            elif type(d[k])==type([]):
+                for k in d[k]:
+                    if type(k)==type({}):
+                        recurse_urls(k)
+
+
+def which_url_next(json_dict):
+
+    global urls_in_resp
+    urls_in_resp = []
+
+    # populates globale variable
+    recurse_urls(json_dict)
+
+    user_input = -1
+    while user_input <= -1 or user_input >= len(urls_in_resp):
+        print ""
+        for idx in xrange(len(urls_in_resp)):
+            print str(idx) + ": " + urls_in_resp[idx]
+        user_input = int(raw_input("-- Which URL to go to (enter number) --\n\n"))
+        print "you entered", user_input
+
+    return urls_in_resp[user_input]
+
+def main(argv): 
+
+    # Hard coding to make calling without args faster 
+    cert_path = "/Users/rirwin/.ssh/collector-gpo-withnpkey.pem"
+    validictory_path = "/Users/rirwin/ops-monitoring/extern/validictory"
+
+    if len(sys.argv) != 2:
+        usage()
+
+    url = sys.argv[1]
+    
+    user_input = 'y'
+
+    while (user_input == 'y'):
+        json_dict = make_request_print_response(url, cert_path)
+        
+        if json_dict:
+            schema = response_validator.parse_schema(json_dict["$schema"])
+            valid = response_validator.validate(json_dict, schema, validictory_path)
+            print "Response from", url, "is",
+            if valid is False:
+                print "*NOT*",
+            print "valid"
+
+        user_input = ' '
+        while (user_input != 'y' and user_input != 'n'):
+            user_input = raw_input("Continue? (y/n)")
+
+        if json_dict and user_input == 'y':            
+            url = which_url_next(json_dict)
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
