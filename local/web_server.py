276ede04783516a6a6ae5ddff8a69b8d7e3c97b6
Stephane Blais <sblais@bbn.com>
2014-10-06 16:39:33 -0400

    Now reporting correct Content-Type in HTTP header.
    Addresses ticket:191.

--- local/web_server.py
+++ local/web_server.py
@@ -22,7 +22,9 @@
 # IN THE WORK.
 #----------------------------------------------------------------------
 
-from flask import Flask, request
+from flask import Flask, request, Response
+import httplib
+
 import sys
 
 class LocalDatastoreServer:
@@ -30,7 +32,7 @@ class LocalDatastoreServer:
         self.local_path = parent_path + "/local/"
         self.common_path = parent_path + "/common/"
         self.config_path = parent_path + "/config/"
-       
+
         self.debug = False
         sys.path.append(self.local_path)
         sys.path.append(self.common_path)
@@ -65,69 +67,72 @@ class LocalDatastoreServer:
 
         self.app = Flask(__name__)
 
+        def make_resp(json_resp):
+            """
+            Function to make an HTTP Response given a json response
+            :param json_resp: the json response in text format
+            :return: an HTTP Response given the json response
+            """
+            return Response(json_resp, status=httplib.OK, mimetype='application/json; charset=utf-8')
 
-        @self.app.route('/info/aggregate/<path:agg_id>', methods = ['GET'])
-        def info_aggregate_args(agg_id): 
-            return rest_call_handler.handle_aggregate_info_query(
-                self.tm, agg_id, self.monitoring_version)
 
-        @self.app.route('/info/externalcheck/<path:extck_id>', methods = ['GET'])
-        def info_externalcheck_args(extck_id): 
-            return rest_call_handler.handle_externalcheck_info_query(
-                self.tm, extck_id)
 
-        @self.app.route('/info/experiment/<path:exp_id>', methods = ['GET'])
-        def info_experiment_args(exp_id): 
-            return rest_call_handler.handle_experiment_info_query(
-                self.tm, exp_id)
+        @self.app.route('/info/aggregate/<path:agg_id>', methods=['GET'])
+        def info_aggregate_args(agg_id):
+            return make_resp(rest_call_handler.handle_aggregate_info_query(self.tm, agg_id, self.monitoring_version))
 
-        @self.app.route('/info/node/<path:node_id>', methods = ['GET'])
-        def info_node_args(node_id): 
-            return rest_call_handler.handle_node_info_query(self.tm, node_id)
-        
-        @self.app.route('/info/interface/<path:iface_id>', methods = ['GET'])
-        def info_interface_args(iface_id): 
-            return rest_call_handler.handle_interface_info_query(
-                self.tm, iface_id)
+        @self.app.route('/info/externalcheck/<path:extck_id>', methods=['GET'])
+        def info_externalcheck_args(extck_id):
+            return make_resp(rest_call_handler.handle_externalcheck_info_query(self.tm, extck_id))
 
-        @self.app.route('/info/interfacevlan/<path:ifacevlan_id>', methods = ['GET'])
-        def info_interfacevlan_args(ifacevlan_id): 
-            return rest_call_handler.handle_interfacevlan_info_query(
-                self.tm, ifacevlan_id)
-        
-        @self.app.route('/info/sliver/<path:sliver_id>', methods = ['GET'])
-        def info_sliver_args(sliver_id): 
-            return rest_call_handler.handle_sliver_info_query(
-                self.tm, sliver_id)
+        @self.app.route('/info/experiment/<path:exp_id>', methods=['GET'])
+        def info_experiment_args(exp_id):
+            return make_resp(rest_call_handler.handle_experiment_info_query(self.tm, exp_id))
 
-        @self.app.route('/info/link/<path:link_id>', methods = ['GET'])
-        def info_link_args(link_id): 
-            return rest_call_handler.handle_link_info_query(self.tm, link_id)
-        
-        @self.app.route('/info/slice/<path:slice_id>', methods = ['GET'])
-        def info_slice_args(slice_id): 
-            return rest_call_handler.handle_slice_info_query(self.tm, slice_id)
-        
-        @self.app.route('/info/user/<path:user_id>', methods = ['GET'])
-        def info_user_args(user_id): 
-            return rest_call_handler.handle_user_info_query(self.tm, user_id)
-        
-        @self.app.route('/info/authority/<path:authority_id>', methods = ['GET'])
-        def info_authority_args(authority_id): 
-            return rest_call_handler.handle_authority_info_query(
-                self.tm, authority_id)
-        
-        @self.app.route('/info/opsconfig/<path:opsconfig_id>', methods = ['GET'])
-        def info_opsconfig_args(opsconfig_id): 
+        @self.app.route('/info/node/<path:node_id>', methods=['GET'])
+        def info_node_args(node_id):
+            return make_resp(rest_call_handler.handle_node_info_query(self.tm, node_id))
+
+        @self.app.route('/info/interface/<path:iface_id>', methods=['GET'])
+        def info_interface_args(iface_id):
+            return make_resp(rest_call_handler.handle_interface_info_query(
+                self.tm, iface_id))
+
+        @self.app.route('/info/interfacevlan/<path:ifacevlan_id>', methods=['GET'])
+        def info_interfacevlan_args(ifacevlan_id):
+            return make_resp(rest_call_handler.handle_interfacevlan_info_query(self.tm, ifacevlan_id))
+
+        @self.app.route('/info/sliver/<path:sliver_id>', methods=['GET'])
+        def info_sliver_args(sliver_id):
+            return make_resp(rest_call_handler.handle_sliver_info_query(self.tm, sliver_id))
+
+        @self.app.route('/info/link/<path:link_id>', methods=['GET'])
+        def info_link_args(link_id):
+            return make_resp(rest_call_handler.handle_link_info_query(self.tm, link_id))
+
+        @self.app.route('/info/slice/<path:slice_id>', methods=['GET'])
+        def info_slice_args(slice_id):
+            return make_resp(rest_call_handler.handle_slice_info_query(self.tm, slice_id))
+
+        @self.app.route('/info/user/<path:user_id>', methods=['GET'])
+        def info_user_args(user_id):
+            return make_resp(rest_call_handler.handle_user_info_query(self.tm, user_id))
+
+        @self.app.route('/info/authority/<path:authority_id>', methods=['GET'])
+        def info_authority_args(authority_id):
+            return make_resp(rest_call_handler.handle_authority_info_query(self.tm, authority_id))
+
+        @self.app.route('/info/opsconfig/<path:opsconfig_id>', methods=['GET'])
+        def info_opsconfig_args(opsconfig_id):
             return rest_call_handler.handle_opsconfig_info_query(
                 self.tm, opsconfig_id)
-        
-        @self.app.route('/data/', methods = ['GET'])
-        def data(): 
+
+        @self.app.route('/data/', methods=['GET'])
+        def data():
             # get everything to the right of ?q= as string from flask.request
             filters = request.args.get('q', None)
-            return rest_call_handler.handle_ts_data_query(self.tm, filters)
-        
+            return make_resp(rest_call_handler.handle_ts_data_query(self.tm, filters))
+
     def __del__(self):
         import logger
         logger.get_logger(self.config_path).critical("Stopping ops monitoring")
@@ -136,4 +141,4 @@ if __name__ == '__main__':
     # We only come here when running directly under Flask, i.e.,
     # when run as "python ./web_server.py"
     server = LocalDatastoreServer('..')
-    server.app.run(debug = True, host = "0.0.0.0", port = 4334)
+    server.app.run(debug=True, host="0.0.0.0", port=4334)
