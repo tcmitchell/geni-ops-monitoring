b4b687dc4cdc5960efbd45eb661555ded68f54c9
Stephane Blais <sblais@bbn.com>
2015-01-07 15:31:19 -0500

    Initial version of script to bring up vagrant VM.
    The intent is to have this script used by jenkins, or a make test target.
    
    Addresses ticket:31.

---
+++ test/vagrant/start-vagrant-vm.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Raytheon BBN Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and/or hardware specification (the "Work") to
+# deal in the Work without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Work, and to permit persons to whom the Work
+# is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
+#
+# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+# IN THE WORK.
+#----------------------------------------------------------------------
+
+# bring up the VM only if it's not up
+ status=$(vagrant status | grep -A 3 "Current machine states:" | grep default | sed 's/default *//')
+ if [ "${status}" != "running (virtualbox)" ]
+ then
+    vagrant up
+ fi
+ 
+ # Always re-provision the VM with the latest code.
+ vagrant provision
