bec7d3ed152c8d017c29b9c6fe1e741623aa7ae6
Stephane Blais <sblais@bbn.com>
2015-02-09 12:44:06 -0500

    Eliminated hard coded information.
    - Transfered info about slices and source ping site to the extck
      configuration file / loader.
    - Broke up the code in methods for readability.
    - Modify how the script changes the python path and loads modules.
      All relative to script path now. Allows for script execution from any
      location now. (not just /usr/local/ops-monitoring/extck)
    
    Addresses ticket:210.

--- extck/coordinate_mesoscale_experiments.py
+++ extck/coordinate_mesoscale_experiments.py
@@ -24,61 +24,97 @@
 import sys
 import os
 import time
-sys.path.append("../common/")
+extck_path = os.path.abspath(os.path.dirname(__file__))
+top_path = os.path.dirname(extck_path)
+common_path = os.path.join(top_path, "common")
+config_path = os.path.join(top_path, "config")
+sys.path.append(common_path)
+sys.path.append(extck_path)
+
 import table_manager
 import opsconfig_loader
+import extck_config
 
-db_type = "local"
-config_path = "../config/"
-# debug = False
+def execute_cmd(cmdStr, opslogger):
+    opslogger.debug("about to execute: " + cmdStr)
+    os.system(cmdStr)
 
-tbl_mgr = table_manager.TableManager(db_type, config_path)
+def sync_up_files(local_path, remote_path, remote_addr, remote_port, ssh_key_file, opslogger):
+    rsyncStr = "rsync -z -e \"ssh -i " + ssh_key_file + " -p " + remote_port + "\" " + local_path + " " + remote_addr + ":" + remote_path
+    execute_cmd(rsyncStr, opslogger)
 
-tbl_mgr.poll_config_store()
-
-ocl = opsconfig_loader.OpsconfigLoader(config_path)
-
-experiment_event_types = ocl.get_event_types()["experiment"]
-
-ipConfigPathLocal = "/usr/local/ops-monitoring/extck/ips.conf"
-ipConfigPathRemote = "/users/amcanary/ips.conf"
-pingerLocal = "/usr/local/ops-monitoring/extck/pinger.py"
-pingerRemote = "/users/amcanary/pinger.py"
-outputFile = "raw_rows"
-outputFileLocal = "/home/amcanary/raw_rows"
-outputFileRemote="/users/amcanary/raw_rows"
-keyPath = "/home/amcanary/.ssh/id_rsa"
-
-
-
-srcPing={"gpo-ig":["amcanary@pc1.instageni.gpolab.bbn.com", "30266"],\
-         "utah-ig": ["amcanary@pc1.utah.geniracks.net", "30010"],\
-         "gpo-ig-3715_core":["amcanary@pc1.instageni.gpolab.bbn.com","31290"],\
-         "gpo-ig-3716_core":["amcanary@pc1.instageni.gpolab.bbn.com", "30522"] }
-
-for site in srcPing:
-    port = srcPing[site][1]
-    # First make sure the src site have the latest ips.conf and pinger.py
-    # using rsync commands to avoid copying over & over the same files.
-    rsyncStr = "rsync -z -e \"ssh -i " + keyPath + " -p " + port + "\" " + ipConfigPathLocal + " " + srcPing[site][0] + ":" + ipConfigPathRemote
-    tbl_mgr.logger.debug("about to execute: " + rsyncStr)
-    os.system(rsyncStr)
-    rsyncStr = "rsync -z -e \"ssh -i " + keyPath + " -p " + port + "\" " + pingerLocal + " " + srcPing[site][0] + ":" + pingerRemote
-    tbl_mgr.logger.debug("about to execute: " + rsyncStr)
-    os.system(rsyncStr)
-    
-    sshStr = "ssh -i " + keyPath + " " + srcPing[site][0] + " -p " + port +\
-               " \"rm -f " + outputFile + " && python pinger.py -o " + outputFile + " -c " + ipConfigPathRemote + " -s " + site + "\""
-    tbl_mgr.logger.debug("about to execute: " + sshStr)
-    os.system(sshStr)
-    scpStr="scp -i " + keyPath + " -P " + port + " " + srcPing[site][0]+ ":" + outputFileRemote + " /home/amcanary"
-    tbl_mgr.logger.debug("about to execute: " + scpStr)
-    os.system(scpStr)
-    file_handle = open(outputFileLocal, 'r')
+def insert_ping_times(outputfile, tbl_mgr, table_str):
+    file_handle = open(outputfile, 'r')
     val_str = ""
+
+    first = True
     for line in file_handle:
-        val_str += line + ","
-    table_str = "ops_experiment_" + experiment_event_types[0]
-    tbl_mgr.insert_stmt(table_str, val_str[:-1])
-    old_ts = int((time.time()-168*60*60)*1000000) # Purge data older than 168 hours (1 wk)
+        # if the ping failed (delay set to -1 in pinger.py) we're not inserting the value.
+        line = line.strip()
+        if line[-3:] != "-1)":
+            if first:
+                first = False
+            else:
+                val_str += ","
+            val_str += line
+    tbl_mgr.insert_stmt(table_str, val_str)
+
+def main(argv):
+    db_type = "local"
+
+    tbl_mgr = table_manager.TableManager(db_type, config_path)
+
+    tbl_mgr.poll_config_store()
+
+    ocl = opsconfig_loader.OpsconfigLoader(config_path)
+
+    experiment_event_types = ocl.get_event_types()["experiment"]
+
+    ext_config = extck_config.ExtckConfigLoader(tbl_mgr.logger)
+
+    ipConfigPathLocal = os.path.join(extck_path, "ips.conf")
+    ipConfigPathRemote = ext_config.get_ips_file_remote_location()
+    pingerLocal = os.path.join(extck_path, "pinger.py")
+    pingerRemote = ext_config.get_pinger_file_remote_location()
+    outputFile = ext_config.get_output_filename()
+    outputFileLocal = os.path.join(ext_config.get_local_output_dir(), outputFile)
+    outputFileRemote = os.path.join(ext_config.get_remote_output_dir(), outputFile)
+    keyPath = ext_config.get_ssh_key_file_location()
+
+
+
+#     srcPing = {"gpo-ig":["amcanary@pc1.instageni.gpolab.bbn.com", "30266"], \
+#              "utah-ig": ["amcanary@pc1.utah.geniracks.net", "30010"], \
+#              "gpo-ig-3715_core":["amcanary@pc1.instageni.gpolab.bbn.com", "31290"], \
+#              "gpo-ig-3716_core":["amcanary@pc1.instageni.gpolab.bbn.com", "30522"] }
+
+    campus_sources = ext_config.get_experiment_source_ping_campus()
+    core_sources = ext_config.get_experiment_source_ping_core()
+
+    all_sources = campus_sources.union(core_sources)
+
+    for site in all_sources:
+        addr = ext_config.get_experiment_source_ping_vm_address(site)
+        port = ext_config.get_experiment_source_ping_vm_port(site)
+        # First make sure the src site have the latest ips.conf and pinger.py
+        # using rsync commands to avoid copying over & over the same files.
+        sync_up_files(ipConfigPathLocal, ipConfigPathRemote, addr, port, keyPath, tbl_mgr.logger)
+        sync_up_files(pingerLocal, pingerRemote, addr, port, keyPath, tbl_mgr.logger)
+
+        sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
+                   " \"rm -f " + outputFileRemote + \
+                   " && python pinger.py -o " + outputFileRemote + " -c " + ipConfigPathRemote + " -s " + site + "\""
+        execute_cmd(sshStr, tbl_mgr.logger)
+
+        scpStr = "scp -i " + keyPath + " -P " + port + " " + addr + ":" + outputFileRemote + " " + outputFileLocal
+        execute_cmd(scpStr, tbl_mgr.logger)
+
+        table_str = "ops_experiment_" + experiment_event_types[0]
+        insert_ping_times(outputFileLocal, tbl_mgr, table_str)
+
+    # Purge data older than 168 hours (1 wk)
+    old_ts = int((time.time() - 168 * 60 * 60) * 1000000)
     tbl_mgr.purge_old_tsdata(table_str, old_ts)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
