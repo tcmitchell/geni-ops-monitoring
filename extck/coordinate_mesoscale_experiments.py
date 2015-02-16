820dd856149579a28af6871c1dc567f8c4144e2e
Stephane Blais <sblais@bbn.com>
2015-02-16 18:31:56 -0500

    Coded parallelization of running ping command at source sites.
    Using a thread pool to execute all the commands related to running
    pings at source sites.
    
    For a given site, synching up the pinger script and IP configurations,
    Executing the ping script, collecting the resulting file, inserting
    the corresponding stats in the DB and deleting the file both locally
    and remotely is executed by one of the threads of the thread pool.
    
    So multiple soucre sites can be engaged simultaneously.
    
    Addresses ticket:218.

--- extck/coordinate_mesoscale_experiments.py
+++ extck/coordinate_mesoscale_experiments.py
@@ -25,6 +25,8 @@ import sys
 import os
 import time
 import tempfile
+import multiprocessing
+import multiprocessing.pool
 
 extck_path = os.path.abspath(os.path.dirname(__file__))
 top_path = os.path.dirname(extck_path)
@@ -37,15 +39,18 @@ import table_manager
 import opsconfig_loader
 import extck_config
 
-def execute_cmd(cmdStr, opslogger):
+
+def execute_cmd(cmdStr, opslogger, lock):
+    lock.acquire()
     opslogger.debug("about to execute: " + cmdStr)
+    lock.release()
     os.system(cmdStr)
 
-def sync_up_files(local_path, remote_path, remote_addr, remote_port, ssh_key_file, opslogger):
+def sync_up_files(local_path, remote_path, remote_addr, remote_port, ssh_key_file, opslogger, lock):
     rsyncStr = "rsync -z -e \"ssh -i " + ssh_key_file + " -p " + remote_port + "\" " + local_path + " " + remote_addr + ":" + remote_path
-    execute_cmd(rsyncStr, opslogger)
+    execute_cmd(rsyncStr, opslogger, lock)
 
-def insert_ping_times(outputfile, tbl_mgr, table_str):
+def insert_ping_times(outputfile, tbl_mgr, table_str, lock):
     file_handle = open(outputfile, 'r')
     val_str = ""
 
@@ -59,9 +64,57 @@ def insert_ping_times(outputfile, tbl_mgr, table_str):
             else:
                 val_str += ","
             val_str += line
+    lock.acquire()
     tbl_mgr.insert_stmt(table_str, val_str)
+    lock.release()
+
+def run_remote_pings((ext_config, site, campus_sources, ipConfigPathLocal,
+                     ipConfigPathRemote, keyPath, pingerLocal, pingerRemote,
+                     poolSize, initialPingCount, measurementPingCount, table_str,
+                     lock, table_mgr)):
+    addr = ext_config.get_experiment_source_ping_vm_address(site)
+    port = ext_config.get_experiment_source_ping_vm_port(site)
+    # First make sure the src site have the latest ips.conf and pinger.py
+    # using rsync commands to avoid copying over & over the same files.
+    sync_up_files(ipConfigPathLocal, ipConfigPathRemote, addr, port, keyPath, table_mgr.logger, lock)
+    sync_up_files(pingerLocal, pingerRemote, addr, port, keyPath, table_mgr.logger, lock)
+
+    remote_output_dir = ext_config.get_remote_output_dir()
+    pref = "extck_tmp_"
+    suff = ".pings"
+    # clean up old tmp file in case some process was interrupted...
+    # delete files older than 60 minutes, matching what we output.
+    sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
+               " \"find " + remote_output_dir + " -mmin +60 -name '" + pref + "*" + suff + "' | xargs rm -vrf" + "\""
+    execute_cmd(sshStr, table_mgr.logger, lock)
+
+    if site in campus_sources:
+        ping_type = 'campus'
+    else:
+        ping_type = 'core'
+
+    (fh, outputFileLocal) = tempfile.mkstemp(suffix=suff, prefix=pref, dir=ext_config.get_local_output_dir())
+    os.close(fh)  # closing tmp file that was just created.
+    filename = os.path.basename(outputFileLocal)
+    outputFileRemote = os.path.join(remote_output_dir, filename)
+    sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
+               " \"rm -f " + outputFileRemote + \
+               " && python pinger.py -o " + outputFileRemote + " -c " + ipConfigPathRemote + " -s " + site + " -t " + ping_type + \
+               " -p " + poolSize + " -i " + initialPingCount + " -m " + measurementPingCount + "\""
+    execute_cmd(sshStr, table_mgr.logger, lock)
+
+    scpStr = "scp -i " + keyPath + " -P " + port + " " + addr + ":" + outputFileRemote + " " + outputFileLocal
+    execute_cmd(scpStr, table_mgr.logger, lock)
+    sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
+               " \"rm -f " + outputFileRemote + "\""
+    execute_cmd(sshStr, table_mgr.logger, lock)
+
+    insert_ping_times(outputFileLocal, table_mgr, table_str, lock)
+    # Delete tmp file
+    os.remove(outputFileLocal)
 
 def main(argv):
+    multiprocessing.util.log_to_stderr(multiprocessing.util.DEBUG)
     db_type = "local"
 
     tbl_mgr = table_manager.TableManager(db_type, config_path)
@@ -79,51 +132,28 @@ def main(argv):
     pingerLocal = os.path.join(extck_path, "pinger.py")
     pingerRemote = ext_config.get_pinger_file_remote_location()
     keyPath = ext_config.get_ssh_key_file_location()
+    poolSize = ext_config.get_experiment_ping_thread_pool_size()
+    initialPingCount = ext_config.get_experiment_ping_initial_count()
+    measurementPingCount = ext_config.get_experiment_ping_measurmentl_count()
 
-
-
-#     srcPing = {"gpo-ig":["amcanary@pc1.instageni.gpolab.bbn.com", "30266"], \
-#              "utah-ig": ["amcanary@pc1.utah.geniracks.net", "30010"], \
-#              "gpo-ig-3715_core":["amcanary@pc1.instageni.gpolab.bbn.com", "31290"], \
-#              "gpo-ig-3716_core":["amcanary@pc1.instageni.gpolab.bbn.com", "30522"] }
 
     campus_sources = ext_config.get_experiment_source_ping_campus()
     core_sources = ext_config.get_experiment_source_ping_core()
 
     all_sources = campus_sources.union(core_sources)
+    table_str = "ops_experiment_" + experiment_event_types[0]
+    myLock = multiprocessing.Lock()
 
+    argsList = []
     for site in all_sources:
-        addr = ext_config.get_experiment_source_ping_vm_address(site)
-        port = ext_config.get_experiment_source_ping_vm_port(site)
-        # First make sure the src site have the latest ips.conf and pinger.py
-        # using rsync commands to avoid copying over & over the same files.
-        sync_up_files(ipConfigPathLocal, ipConfigPathRemote, addr, port, keyPath, tbl_mgr.logger)
-        sync_up_files(pingerLocal, pingerRemote, addr, port, keyPath, tbl_mgr.logger)
-
-        if site in campus_sources:
-            ping_type = 'campus'
-        else:
-            ping_type = 'core'
-        (fh, outputFileLocal) = tempfile.mkstemp(suffix=".pings", prefix="extck_tmp_", dir=ext_config.get_local_output_dir())
-        os.close(fh)  # closing tmp file that was just created.
-        filename = os.path.basename(outputFileLocal)
-        outputFileRemote = os.path.join(ext_config.get_remote_output_dir(), filename)
-        sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
-                   " \"rm -f " + outputFileRemote + \
-                   " && python pinger.py -o " + outputFileRemote + " -c " + ipConfigPathRemote + " -s " + site + " -t " + ping_type + "\""
-        execute_cmd(sshStr, tbl_mgr.logger)
-
-        scpStr = "scp -i " + keyPath + " -P " + port + " " + addr + ":" + outputFileRemote + " " + outputFileLocal
-        execute_cmd(scpStr, tbl_mgr.logger)
-        sshStr = "ssh -i " + keyPath + " " + addr + " -p " + port + \
-                   " \"rm -f " + outputFileRemote + "\""
-        execute_cmd(sshStr, tbl_mgr.logger)
-
-        table_str = "ops_experiment_" + experiment_event_types[0]
-        insert_ping_times(outputFileLocal, tbl_mgr, table_str)
-        # Delete tmp file
-        os.remove(outputFileLocal)
-
+        args = (ext_config, site, campus_sources,
+                         ipConfigPathLocal, ipConfigPathRemote, keyPath,
+                         pingerLocal, pingerRemote,
+                         poolSize, initialPingCount, measurementPingCount,
+                         table_str, myLock, tbl_mgr)
+        argsList.append(args)
+    pool = multiprocessing.pool.ThreadPool(processes=4)
+    pool.map(run_remote_pings, argsList)
     # Purge data older than 168 hours (1 wk)
     old_ts = int((time.time() - 168 * 60 * 60) * 1000000)
     tbl_mgr.purge_old_tsdata(table_str, old_ts)
